a7b8f87d0c339f65c3c3fa8d71311c00d96c3027
Richard Enric Villanera Quiroga <36244098+richardenric@users.noreply.github.com>
2019-03-01 17:59:55 +0100

    Versión 1.0
    Archivos del trabajo del año 2017/2018

---
+++ Plataforma.h
@@ -0,0 +1,17 @@
+//
+//  Plataforma.h
+//  JuegoSnake
+//
+//  Created by Richard villanera quiroga on 25/4/18.
+//  Copyright © 2018 Richard villanera quiroga. All rights reserved.
+//
+
+#ifndef Plataforma_h
+#define Plataforma_h
+class Plataforma
+{
+    Plataforma();
+    ~Plataforma();
+};
+
+#endif /* Plataforma_h */
